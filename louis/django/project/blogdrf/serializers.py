e3c086920cd9879a6e9ff09924a7697c977403a0
dagusen <louisaleksieb.dagusen@gmail.com>
2018-05-02 14:35:54 +0800

    Update post, tag, category serializers add url

--- louis/django/project/blogdrf/serializers.py
+++ louis/django/project/blogdrf/serializers.py
@@ -2,17 +2,40 @@ from rest_framework import serializers
 
 from .models import Category, Tag, Post
 
-class CategorySerializer(serializers.HyperlinkedModelSerializer):
+class CategorySerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name="category-detail")
     class Meta:
         model = Category
-        fields = ('title', 'date_created', 'date_modified')
+        fields = (
+            'url',
+            'title',
+            'date_created',
+            'date_modified'
+        )
 
-class TagSerializer(serializers.HyperlinkedModelSerializer):
+class TagSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name="tag-detail")
     class Meta:
         model = Tag
-        fields = ('title', 'date_created', 'date_modified')
+        fields = (
+            'title',
+            'date_created',
+            'date_modified'
+        )
 
-class PostSerializer(serializers.HyperlinkedModelSerializer):
+class PostSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name="post-detail")
     class Meta:
         model = Post
-        fields = ('title', 'subtitle', 'banner_photo', 'tags', 'category', 'body', 'status')
+        fields = (
+            'url',
+            'title',
+            'subtitle',
+            'banner_photo',
+            'tags',
+            'category',
+            'body',
+            'status',
+            'date_created',
+            'date_modified'
+        )
--- louis/django/project/blogdrf/views.py
+++ louis/django/project/blogdrf/views.py
@@ -18,14 +18,32 @@ class CategoryViewSet(viewsets.ViewSet):
         serializer = CategorySerializer(queryset, many=True)
         return Response(serializer.data)
 
+    def retrieve(self, request, pk=None):
+        queryset = Category.objects.all()
+        category = get_object_or_404(queryset, pk=pk)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+
 class TagViewSet(viewsets.ViewSet):
     def list(self ,request):
         queryset = Tag.objects.all()
         serializer = TagSerializer(queryset, many=True)
         return Response(serializer.data)
 
+    def retrieve(self, request, pk=None):
+        queryset = Tag.objects.all()
+        tag = get_object_or_404(queryset, pk=pk)
+        serializer = TagSerializer(tag)
+        return Response(serializer.data)
+
 class PostViewSet(viewsets.ViewSet):
     def list(self ,request):
         queryset = Post.objects.all()
-        serializer = PostSerializer(queryset, many=True)
+        serializer = PostSerializer(queryset, many=True, context={'request': request})
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Post.objects.all()
+        post = get_object_or_404(queryset, pk=pk)
+        serializer = PostSerializer(post, context={'request': request})
         return Response(serializer.data)
