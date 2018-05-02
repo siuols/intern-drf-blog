1d33e24b67847e14bd5346762acbcdfd68c17cca
dagusen <louisaleksieb.dagusen@gmail.com>
2018-05-02 10:30:56 +0800

    Add Tag, Category, and Post models

---
+++ louis/django/project/blogdrf/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+# Create your models here.
+
+class Tag(models.Model):
+    title               = models.CharField(max_legth=255)
+    date_created        = models.DateTimeField(auto_now_add=True)
+    date_modified       = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '{}'.format(self.title)
+
+class Category(models.Model):
+    title               = models.CharField(max_legth=255)
+    date_created        = models.DateTimeField(auto_now_add=True)
+    date_modified       = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '{}'.format(self.title)
+
+STATUS_CHOICES =(
+        ('published', 'Published'),
+        ('draft', 'Draft'),
+        ('hidden', 'Hidden')
+    )
+
+class Post(models.Model):
+    title               = models.CharField(max_legth=255)
+    subtitle            = models.CharField(max_legth=255)
+    banner_photo        = models.ImageField()
+    tags                = models.ManyToManyField(Tag)
+    categoty            = models.ForiegnKey('Category', on_delete=models.CASCADE)
+    body                = models.TextField()
+    status              = models.CharField(
+                                            max_legth=9,
+                                            choices=STATUS_CHOICES,
+                                            default='published'
+                                        )
+    date_created        = models.DateTimeField(auto_now_add=True)
+    date_modified       = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '{}'.format(self.title)
